bb320d560e9845083a4785be774ed39867c77037
paschal-droid <67451397+paschal-droid@users.noreply.github.com>
2021-04-19 21:30:04 +0100

    add initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+// $(".nav-link").each(function () {
+//     $(".nav-link").click(function () {
+//         $(".nav-link").css("border-bottom", "3px solid white" )
+//     })
+// })
+
+// $(this.Attr("id")).click(function (params) {
+//     this.css({"opacity": "0.5"})
+// // })
